bd0be5afb611a058251ce491c759778c4495324d
Jappananny <Jappananny@gmail.com>
2022-10-02 14:51:07 +0300

    ДЗ3-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        System.out.println("Задача 1");
+
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Ура ! Тебе " + age + " Поздравляю! ");
+        }
+        if (age < 18) {
+            System.out.println("Упс ! Тебе надо еще подождать ");
+        }
+        //Вариант 2
+        System.out.println("Вариант 2");
+        int age1 = 14;
+        if (age1 >= 18) {
+            System.out.println("Ура ! Тебе 18 Поздравляю! ");
+        }
+        if (age1 < 18) {
+            System.out.println("Упс ! Тебе " + age1 + ", надо еще подождать ");
+        }
+
     }
 }
