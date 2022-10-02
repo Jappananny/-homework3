f595d00e95711a566973eb444dea6446851bb319
Jappananny <Jappananny@gmail.com>
2022-10-02 15:03:54 +0300

    ДЗ3-2

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,21 @@ public class Main {
         if (age1 < 18) {
             System.out.println("Упс ! Тебе " + age1 + ", надо еще подождать ");
         }
+        //Задача 2
+        System.out.println("Задача 2");
+        int age2 = 34;
+        int schoolAge = 7;
+        int universityAge = 18;
+        int workAge = 24;
 
+        if (age2 == schoolAge) {
+            System.out.println("Пора идти в школу, получать образование");
+        }
+        if (age2 == universityAge) {
+            System.out.println("Пора в универ! ");
+        }
+        if (age2 >= workAge) {
+            System.out.println("Пора искать работу! ");
+        }
     }
 }
